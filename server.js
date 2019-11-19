39a4406262ebee7e6fc798e4dbe39bcd730e3419
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2019-11-19 00:58:10 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const url = require("url");
+const fs = require("fs");
+const http = require("http");
+
+http.createServer((Request, Response) => {
+    let addr = request.url;
+    let q = url.parse(addr, true);
+    let filepath = "";
+
+    Response.writeHead(200, { "Content-Type": "text/plain" });
+    Response.end("Hello Node!\n");
+
+    if (q.pathname.includes("documentation")) {
+        filepath = (__dirname + "/documentation.html");
+    } else {
+        filepath = "index.html";
+    }
+
+    fs.appendFile("log.txt", "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n", function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Added to log.");
+        }
+    });
+}).listen(8080);
+
+console.log("My first Node test server is running on Port 8080.");
