b07e46e46e21122d2d792a5b16941a588aee79f3
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2019-11-24 00:09:21 -0800

    trying out morgan

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const express = require("express");
+const express = require('express'),
+  morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', function (req, res) {
+  res.send('Welcome to my app!');
+});
+app.get('/secreturl', function (req, res) {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080);
